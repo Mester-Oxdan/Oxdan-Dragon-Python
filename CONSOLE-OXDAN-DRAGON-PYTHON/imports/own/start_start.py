d0e73011ba3aa05ba544add6984f77b09ec3a47c
Mester re <bogdan2010gladiy@gmail.com>
2023-08-31 19:50:02 -0400

    small fix

--- CONSOLE-OXDAN-DRAGON-PYTHON/imports/own/start_start.py
+++ CONSOLE-OXDAN-DRAGON-PYTHON/imports/own/start_start.py
@@ -39,54 +39,8 @@ import git
 import requests
 import shutil
 import time
-from winotify import Notification, audio
 
 start_time = time.time()
-
-def get_latest_commit_hash(username, repo, branch):
-    url = f"https://api.github.com/repos/{username}/{repo}/branches/{branch}"
-    response = requests.get(url)
-    if response.status_code == 200:
-        return response.json()["commit"]["sha"]
-    return None
-
-def get_current_commit_hash(repo_path):
-    repo = git.Repo(repo_path)
-    return repo.head.commit.hexsha
-
-def update_program(username, repo, branch, local_dir):
-    try:
-        remote_commit_hash = get_latest_commit_hash(username, repo, branch)
-        local_commit_hash = get_current_commit_hash(local_dir)
-
-        if remote_commit_hash and local_commit_hash and remote_commit_hash != local_commit_hash:
-            #print("A new version is available.")
-            message = Notification(app_id="OXDAN-DRAGON-PYTHON",
-                                   title="New Update!",
-                                   msg="New update available. After login use 'update' command.",
-                                   duration="short",
-                                   icon=os.path.join(os.environ["OXDAN-DRAGON-PYTHON"],"imports/own/my_dragon_ico.jpg"))
-            
-            message.set_audio(audio.Default, loop=False)
-
-            message.show()
-            #shutil.rmtree(local_dir)  # Remove the existing directory
-            #git.Repo.clone_from(f"https://github.com/{username}/{repo}.git", local_dir)
-            
-            #print("Program updated successfully.")
-        #else:
-            #print("You have the latest version.")
-
-    except Exception as e:
-        print("Error:", e)
-        msvcrt.getch()
-        
-github_username = "Mester-Oxdan"
-repository_name = "Oxdan-Dragon-Python"
-target_branch = "main"  # Update this with the branch you want to update from
-local_directory = "../"  # Update this with your local program directory
-    
-update_program(github_username, repository_name, target_branch, local_directory)
 
 def printMenu(items, active_index):
 
@@ -116,6 +70,7 @@ def start_start():
     #print(os.path.join(os.environ["OXDAN-DRAGON-PYTHON"],"imports/own/resources/music/steel.ogg"))
     #print(os.path.join(os.environ["OXDAN-DRAGON-PYTHON"], "resources\math_game\kenvector_future.ttf"))
     #print(os.path.join(os.environ["OXDAN-DRAGON-PYTHON"],"imports/own/config.txt"))
+    #print(os.path.join(os.environ["OXDAN-DRAGON-PYTHON"],"imports/own/my_dragon_ico.jpg"))
     #msvcrt.getch()
 
     Menu = ["                                                              REGISTRATION", "                                                              LOGIN", "                                                              INSTRUCTIONS", "                                                              EXIT"]
--- CONSOLE-OXDAN-DRAGON-PYTHON/imports/own/will_go_to_start.py
+++ CONSOLE-OXDAN-DRAGON-PYTHON/imports/own/will_go_to_start.py
@@ -11,6 +11,8 @@ import imports.own.Own
 import imports.own.Pranks
 import imports.own.Elses
 from colorama import Fore
+from winotify import Notification, audio
+import requests, git, msvcrt
 
 global which1
 which1 = True
@@ -19,6 +21,57 @@ global yes_or_no
 yes_or_no = True
 
 input_list = []
+
+def get_latest_commit_hash(username, repo, branch):
+    url = f"https://api.github.com/repos/{username}/{repo}/branches/{branch}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()["commit"]["sha"]
+    return None
+
+def get_current_commit_hash(repo_path):
+    repo = git.Repo(repo_path)
+    return repo.head.commit.hexsha
+
+#print(os.path.join(os.environ["OXDAN-DRAGON-PYTHON"],r"my_dragon_ico_transformed.png"))
+#msvcrt.getch()
+
+def update_program(username, repo, branch, local_dir):
+    try:
+        remote_commit_hash = get_latest_commit_hash(username, repo, branch)
+        local_commit_hash = get_current_commit_hash(local_dir)
+
+        if remote_commit_hash and local_commit_hash and remote_commit_hash != local_commit_hash:
+            #print("A new version is available.")
+            message = Notification(app_id="OXDAN-DRAGON-PYTHON",
+                                   title="New Update!",
+                                   msg="New update available. After login use 'update' command.",
+                                   duration="short",
+                                   icon=os.path.join(os.environ["OXDAN-DRAGON-PYTHON"],r"my_dragon_ico_transformed.png"))
+                                   #icon=r"C:\Users\bogda\Downloads\Oxdan-Dragon-Python\CONSOLE-OXDAN-DRAGON-PYTHON\imports\own\my_dragon_ico_transformed.png")
+                                   
+            
+            message.set_audio(audio.Default, loop=False)
+            message.add_actions(label="Go to Website.", launch="https://github.com/Mester-Oxdan/Oxdan-Dragon-Python")
+
+            message.show()
+            #shutil.rmtree(local_dir)  # Remove the existing directory
+            #git.Repo.clone_from(f"https://github.com/{username}/{repo}.git", local_dir)
+            
+            #print("Program updated successfully.")
+        #else:
+            #print("You have the latest version.")
+
+    except Exception as e:
+        print("Error:", e)
+        msvcrt.getch()
+        
+github_username = "Mester-Oxdan"
+repository_name = "Oxdan-Dragon-Python"
+target_branch = "main"  # Update this with the branch you want to update from
+local_directory = "../"  # Update this with your local program directory
+    
+update_program(github_username, repository_name, target_branch, local_directory)
 
 def main():
 
