60bc0cdb3951ab2d2a968e2d74fe8cd4c990746b
Mester re <bogdan2010gladiy@gmail.com>
2023-09-25 16:58:11 -0400

    small change

--- CONSOLE-OXDAN-DRAGON-PYTHON/imports/own/Hacker_Stuffs.py
+++ CONSOLE-OXDAN-DRAGON-PYTHON/imports/own/Hacker_Stuffs.py
@@ -19,6 +19,22 @@ from imports.own.get_ip_site_start67 import get_ip_site_start
 from imports.own.morse_code_start import morse_code_start
 from imports.own.cuaser_cipher_start import cuaser_cipher_start
 from imports.own.con_wifi_start import con_wifi_start
+from msvcrt import getche
+from time import sleep
+import phonenumbers
+from phonenumbers import timezone
+from phonenumbers import geocoder
+from phonenumbers import carrier
+ 
+import folium
+ 
+from opencage.geocoder import OpenCageGeocode
+from phonenumbers.util import u
+import pycountry , phonenumbers
+from phonenumbers.phonenumberutil import region_code_for_number
+from geopy.geocoders import Nominatim
+from uszipcode import SearchEngine
+from colorama import Fore
 
 STD_OUTPUT_HANDLE = -11
 hStdOut = ctypes.windll.kernel32.GetStdHandle(STD_OUTPUT_HANDLE)
@@ -142,6 +158,57 @@ def Hacker_Stuffs():
         except:
 
             print(Fore.RED + "\n(!ERROR!) " + Fore.WHITE + "=" + Fore.GREEN + " (!Enter network ip address!)" + Fore.WHITE)
+            imports.own.will_go_to_start.main()
+            
+    if imports.own.will_go_to_start.x.lower() == "phone_search": # phone_search (+)
+
+        try:
+
+            #ip_search()
+            # taking input the phonenumber along with the country code
+            print(" ")
+            print(Fore.RED + "Write 'esc' (for exit)")
+            number = input(Fore.YELLOW + "Enter Phone Number with country code like (+14129089359): ")
+
+            print(Fore.WHITE)
+
+            if number == "esc":
+    
+                imports.own.will_go_to_start.main()
+
+            # Parsing the phonenumber string to convert it into phonenumber format
+            phoneNumber = phonenumbers.parse(number)
+ 
+            # Storing the API Key in the Key variable
+            Key = "4ce6fc5fb55a4e0883a37f1513389bf2" #generate your api https://opencagedata.com/api
+ 
+            # Using the geocoder module of phonenumbers to print the Location in console
+            yourLocation = geocoder.description_for_number(phoneNumber,"en")
+            region = yourLocation
+            country = pycountry.countries.get(alpha_2 = region_code_for_number(phoneNumber))
+            city = "test"
+            zip_cor = "test"
+            not_sure = "(No info)"
+
+            print(" ") 
+            print("Country: " + country.name)
+            sleep(0.01)
+            print("Region: " + region)
+            sleep(0.01)
+            print("City: " + not_sure)
+            sleep(0.01)
+            print("Zip: " + not_sure)
+            sleep(0.01)
+            print("Y: " + not_sure)
+            sleep(0.01)
+            print("X: " + not_sure)
+            sleep(0.01)
+            #print(" ")
+            imports.own.will_go_to_start.main()
+
+        except:
+
+            print(Fore.RED + "\n(!ERROR!) " + Fore.WHITE + "=" + Fore.GREEN + " (!Enter Phone number with country code!)" + Fore.WHITE)
             imports.own.will_go_to_start.main()
 
     if imports.own.will_go_to_start.x.lower() == "wifi_hack": # wifi_hack (+)
